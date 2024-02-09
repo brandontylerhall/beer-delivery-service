b4cf06d77ee794cae1396ded214e025ea6255aac
Brandon Hall <bran.tyler.hall@gmail.com>
2024-02-09 15:26:30 -0500

    created empty shell for the most basic of all basic text games. created mods for rooms and main.

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+    user_input = "> "
+
+    print("----------------Welcome to Simple Game \U0001F642 Nice to have you----------------")
+    print()
+    print()
+    print("You're in the LIVING ROOM. Your DAD is on the COUCH watching Fox News.\n"
+          "The KITCHEN is to your LEFT and your ROOM is to the RIGHT.\n")
+
+    input(user_input)
+
+
+main()
