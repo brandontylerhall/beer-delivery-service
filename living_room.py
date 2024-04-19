a2fd23691263f3a38503db17758cb9a0020dd5d1
Brandon Hall <bran.tyler.hall@gmail.com>
2024-04-19 10:42:29 -0400

    i don't know how to implement modules yet, or if it's even needed, so i deleted them for now

--- living_room.py
+++
@@ -1,7 +0,0 @@
-def living_room():
-    directions = ['left', 'right']
-    print("You're in the LIVING ROOM. Your DAD is on the COUCH watching Fox News.\n"
-          "The KITCHEN is to your LEFT and your ROOM is to the RIGHT.\n")
-
-
-living_room()
